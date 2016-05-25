c1f8cbd13742444d095f4d3d9cf7faebb2041997
148100 <148100@ISDPF00PJQ8>
2016-05-25 17:29:52 +0800

    Add project files.

---
+++ MVCtraining/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCtraining.Startup))]
+namespace MVCtraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
